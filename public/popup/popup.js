d5b5eba08e3e12e20b260a74516683fe7ce59135
Oguz Gelal <o.gelal77@gmail.com>
2014-11-24 17:46:30 +0200

    Fixed chrome popup iframe source

--- public/popup/popup.js
+++ public/popup/popup.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
-  var baseUrl = "http://localhost:3000";
+  var baseUrl = "http://localhost:3000/chat/";
   var passUrl = "";
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     passUrl = tabs[0].url;
     passUrl = encodeURIComponent(passUrl);
-    var url = baseUrl+"#"+passUrl;
+    console.log(passUrl);
+    var url = baseUrl+passUrl;
     $('iframe').prop('src', url);
   });
 
