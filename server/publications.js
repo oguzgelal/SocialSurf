555acbb5883236598dd6756141711669e7cd4629
Oguz Gelal <o.gelal77@gmail.com>
2014-11-26 02:49:04 +0200

    Refactor and move

--- server/publications.js
+++ server/publications.js
@@ -1,27 +1,6 @@
 Meteor.publish('rooms', function(url){
-
-  /*
-  // Client Connected
-  console.log("Client Connected...");
-  Rooms.update({url: url}, {$inc: {online: 1}});
-  if(this._session.socket._events.data.length === 1) {
-  console.log(this);
-  this._session.socket.on("close", Meteor.bindEnvironment(function() {
-  // Client Disconnected
-  console.log("Client Disconnected...");
-  Rooms.update({url: url}, {$inc: {online: -1}});
-},
-function(e) {
-console.log("Connection Close Error : ", e);
-}));
-}
-*/
-return Rooms.find({url: url});
+  return Rooms.find({url: url});
 });
-
-//Meteor.publish('onlineStats', function(url){
-//  return OnlineStats.find({url: url});
-//});
 
 Meteor.publish('messages', function(roomid){
   return Messages.find({roomid: roomid});
