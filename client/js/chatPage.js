f7516c53c5e9772b1367cb007bb1143d6268e4ff
Oguz Gelal <o.gelal77@gmail.com>
2014-12-01 19:10:20 +0200

    settings bar animations and toggle completed

--- client/js/chatPage.js
+++ client/js/chatPage.js
@@ -14,6 +14,12 @@ Template.chatPage.helpers({
   }
 });
 
+// Move the settings bar upside of the screen according to its height
+Template.chatPage.rendered = function() {
+  var h = $('.settingsBar').height();
+  $('.settingsBar').css("margin-top", -h+"px");
+}
+
 Template.chatPage.events({
   "click .sendMessage": function(event, template){
     var ths = this;
@@ -25,47 +31,58 @@ Template.chatPage.events({
   },
 
   "mouseenter .settingsBarToggle": function(event, template){
-    if (!$('.settingsBar').hasClass('active')){
-      $('.settingsBarToggle').css("padding-top", "15px");
-      $('.settingsBar').css("top", "-42%");
-    }
-    else{
-      $('.settingsBar').css("top", "-5px");
-      var toggleTop = $('.settingsBarToggle').css("top");
-      $('.settingsBarToggle').css("margin-top", "-5px");
-      $('.settingsBarToggle').css("padding-top", "5px");
-    }
+    settingsBarAnimate("down");
   },
   "mouseleave .settingsBarToggle": function(event, template){
-    if (!$('.settingsBar').hasClass('active')){
-      $('.settingsBarToggle').css("padding-top", "10px");
-      $('.settingsBar').css("top", "-44%");
-    }
-    else{
-      $('.settingsBar').css("top", "0");
-      $('.settingsBarToggle').css("margin-top", "0");
-      $('.settingsBarToggle').css("padding-top", "10px");
-    }
+    settingsBarAnimate("up");
   },
   "click .settingsBarToggle": function(event, template){
-    $('.settingsBarToggle').css("margin-top", "0");
-    if (!$('.settingsBar').hasClass('active')){
-      toggleSettingsBarIconTo("close");
-      $('.settingsBar').addClass('active');
-      var settingsBarHeight = $('.settingsBar').height();
-      $('.settingsBarToggle').css("top", settingsBarHeight+"px");
-      $('.settingsBar').css("top", "0");
-    }
-    else{
-      toggleSettingsBarIconTo("settings");
-      $('.settingsBar').removeClass('active');
-      $('.settingsBar').css("top", "-44%");
-      $('.settingsBarToggle').css("top", "0");
-    }
-
+    settingsBarToggle();
   }
 });
 
+// Slight move of the settingsBarLine and settingsBarToggle
+function settingsBarAnimate(str){
+  if (str == "down" && !isSettingBarOpen()){
+    $('.settingsBarToggle').css("padding-top", "17px");
+    $('.settingsBarLine').height(10);
+  }
+  else if (str == "up" && !isSettingBarOpen()){
+    $('.settingsBarToggle').css("padding-top", "10px");
+    $('.settingsBarLine').height(7);
+  }
+}
+
+// Toggle settingsBar
+function settingsBarToggle(){
+  if (isSettingBarOpen()){ setSettingsBarState("close"); }
+    else{ setSettingsBarState("open");
+  }
+}
+
+// Open or Close the settingsBar
+function setSettingsBarState(str){
+  if (str == "open" && !isSettingBarOpen()){
+    toggleSettingsBarIconTo("close");
+    $('.settingsBar').addClass('active');
+    $('.settingsBarLine').fadeOut('fast');
+    $('.settingsBar').css("margin-top", "0px");
+  }
+  else if (str == "close" && isSettingBarOpen()){
+    toggleSettingsBarIconTo("settings");
+    $('.settingsBar').removeClass('active');
+    $('.settingsBarLine').show();
+    var h = $('.settingsBar').height();
+    $('.settingsBar').css("margin-top", -h+"px");
+  }
+}
+
+// Returns true if settingsBar is open
+function isSettingBarOpen(){
+  return $('.settingsBar').hasClass('active');
+}
+
+// Toggle the settingsBarToggle icon to settings or close icons
 function toggleSettingsBarIconTo(str){
   if (str == "close"){
     $('.settingsBarToggle .settingsIcon').hide(0, function(){
