ef425f373bf1213cbdcc6c78c834005f3dcedca8
Oguz Gelal <o.gelal77@gmail.com>
2015-06-28 19:43:22 +0300

    design of the settings bar and message input area fixed. send button animation added

---
+++ client/js/frames/frame.js
@@ -0,0 +1,190 @@
+Meteor.startup(function() {
+  Meteor.setInterval(function(){
+    Session.set('time', new Date);
+  }, 1000);
+});
+
+Template.frame.helpers({
+  messages: function(){ return Messages.find({},{sort:{date:1}}); },
+  nickname: function(){ return amplify.store("nickname"); },
+  online: function(){ return Online.find().count(); },
+  getName: function(){
+    if (Meteor.user()){
+      var userCurrent = Members.findOne({_id: Meteor.user()._id});
+      return userCurrent.name;
+    }
+  },
+  getAvatar: function(){
+    if (Meteor.user()){
+      var userCurrent = Members.findOne({_id: Meteor.user()._id});
+      return userCurrent.avatar;
+    }
+  }
+});
+
+Template.messageBox.helpers({
+  timePassed: function(date){
+    var now = Session.get('time') || new Date;
+    var diff = now.getTime() - date.getTime();
+    return Utils.mstostr(diff);
+  },
+  hasUser: function(){ return this.user; },
+
+  // if the previous message belongs to the same user,
+  // merge the message with the line above
+  mergeLastMessage: function(){
+    var currentUser = this.user;
+    var currentNick = this.nick;
+    var messagesArray = Messages.find({}).fetch();
+    // !!!!!! TODO !!!!!!!
+    // Create a field in rooms table and store the latest id or nickname ???
+    // !!!!!! TODO !!!!!!!
+    for(var i = 0; i < messagesArray.length; i++){
+      if (messagesArray[i]._id == this._id){
+        if (!messagesArray[i-1]){ return false; }
+        else{
+          if (messagesArray[i-1].user){
+            if (!currentUser){ return false; }
+            return messagesArray[i-1].user._id == currentUser._id;
+          }
+          else{
+            if (currentUser){ return false; }
+            return messagesArray[i-1].nick == currentNick;
+          }
+        }
+      }
+    }
+    return false;
+  }
+});
+
+Template.messageBox.rendered = function(){
+  $('.messageScrollable').scrollTop($('.messageArea').prop("scrollHeight"));
+}
+
+Template.frame.rendered = function() {
+  var ths = this;
+  // Move the settings bar upside of the screen according to its height
+  var h = $('.settingsBar').height();
+  $('.settingsBar').css("margin-top", -h + "px");
+  // scroll to the bottom
+  $('.messageScrollable').scrollTop($('.messageArea').prop("scrollHeight"));
+}
+
+Template.frame.events({
+  // SETTINGS BAR EVENTS ------------------------------------
+  "focus .settingsNick": function(event, template){ $(".settingsNick").select(); },
+  "click .btn-logout": function(event, template){ Meteor.logout(); },
+  "click .btn-facebook": function(event, template){ Login.facebook(); },
+  "click .btn-twitter": function(event, template){ Login.twitter(); },
+  "click .btn-google": function(event, template){ Login.google(); },
+  "mouseup .settingsNick": function(event, template){ event.preventDefault(); },
+  "click .btn-setnick": function(event, template){ setNickname($('.settingsNick').val()); },
+  "keypress .settingsNick": function(event, template) {
+    if (event.which == 13){ setNickname($('.settingsNick').val()); }
+  },
+  "mouseenter .settingsBarToggle": function(event, template){ settingsBarAnimate("down"); },
+  "mouseleave .settingsBarToggle": function(event, template){ settingsBarAnimate("up"); },
+  "click .settingsBarToggle": function(event, template){ settingsBarToggle(); },
+  // MESSAGE AREA EVENTS ------------------------------------
+  // MESSAGE EVENTS -----------------------------------------
+  "click .sendButton": function(event, template) {
+    var ths = this;
+    var message = $('.messageInputText')[0].value;
+    sendMessage(ths, message);
+  },
+  "keydown .messageInputText": function(event, template) {
+    var ths = this;
+    if (event.keyCode == 13 && !event.shiftKey) {
+      event.preventDefault();
+      var msg = $('.messageInputText').val();
+      sendMessage(ths, msg);
+      return false;
+    }
+  }
+});
+
+// Slight move of the settingsBarLine and settingsBarToggle
+function settingsBarAnimate(str) {
+  if (str == "down" && !isSettingBarOpen()) {
+    $('.settingsBarToggle').css("padding-top", "17px");
+    $('.settingsBarLine').height(12);
+  } else if (str == "up" && !isSettingBarOpen()) {
+    $('.settingsBarToggle').css("padding-top", "10px");
+    $('.settingsBarLine').height(10);
+  }
+}
+
+// Toggle settingsBar
+function settingsBarToggle() {
+  if (isSettingBarOpen()) { setSettingsBarState("close"); }
+  else { setSettingsBarState("open"); }
+}
+
+// Open or Close the settingsBar
+function setSettingsBarState(str) {
+  if (str == "open" && !isSettingBarOpen()) {
+    toggleSettingsBarIconTo("close");
+    $('.settingsBar').addClass('active');
+    $('.settingsBarLine').fadeOut('fast');
+    $('.settingsBar').css("margin-top", "0px");
+  } else if (str == "close" && isSettingBarOpen()) {
+    toggleSettingsBarIconTo("settings");
+    $('.settingsBar').removeClass('active');
+    $('.settingsBarLine').show();
+    var h = $('.settingsBar').height();
+    $('.settingsBar').css("margin-top", -h + "px");
+  }
+}
+
+// Returns true if settingsBar is open
+function isSettingBarOpen() { return $('.settingsBar').hasClass('active'); }
+
+// Toggle the settingsBarToggle icon to settings or close icons
+function toggleSettingsBarIconTo(str) {
+  if (str == "close") {
+    $('.settingsBarToggle .settingsIcon').hide(0, function() {
+      $('.settingsBarToggle .closeIcon').show('fast');
+      Session.set('frame-toggle-bg', $('.settingsBarToggle').css('background-color'));
+      $('.settingsBarToggle').css('background-color', 'transparent');
+    });
+  } else if (str == "settings") {
+    $('.settingsBarToggle .closeIcon').hide(0, function() {
+      $('.settingsBarToggle .settingsIcon').show('fast');
+      $('.settingsBarToggle').css('background-color', Session.get('frame-toggle-bg'));
+    });
+  }
+}
+
+// Set nickname and store it to persistent session var
+function setNickname(str) {
+  amplify.store("nickname", str);
+  setSettingsBarState("close");
+}
+
+// Send the message
+function sendMessage(ths, message){
+  if (message.length > 0){
+    // animate send icon
+    $('.sendButton').css("bottom", "48px");
+    $('.sendButton').css("right", "-23px");
+    $('.sendButton').css("text-shadow", "0 5px 5px gray");
+    $('.sendButton').css("visibility", "hidden");
+    setTimeout(function(){
+      $('.sendButton').css("bottom", "-50px");
+      $('.sendButton').css("right", "50px");
+      setTimeout(function(){
+        $('.sendButton').css("visibility", "visible");
+        $('.sendButton').css("bottom", "-3px");
+        $('.sendButton').css("right", "18px");
+        $('.sendButton').css("text-shadow", "none");
+      },200);
+    },200);
+    // send message
+    var nick = amplify.store("nickname");
+    var userSent = null;
+    if (Meteor.user()){ userSent = Members.findOne({_id: Meteor.user()._id}); }
+    Meteor.call("addMessage", ths._id, message, nick, userSent);
+    $('.messageInputText').val("");
+  }
+}
---
+++ client/js/utils.js
@@ -0,0 +1,58 @@
+Login = {
+	facebook: function(){
+		Meteor.loginWithFacebook({
+			loginStyle: "popup",
+			requestPermissions: ['public_profile', 'email', 'user_friends']
+		}, function(error){
+			if(error){ console.log("Cannot connect to facebook."); }
+			else{ console.log("Logged in:"); console.log(Meteor.user()); }
+		});
+	},
+	twitter: function(){
+		Meteor.loginWithTwitter({
+			loginStyle: "popup"
+		}, function(error){
+			if(error){ console.log("Cannot connect to twitter."); }
+			else{ console.log("Logged in:"); console.log(Meteor.user()); }
+		});
+	},
+	google: function(){
+		Meteor.loginWithGoogle({
+			loginStyle: "popup",
+			requestPermissions: ['openid', 'email', 'profile']
+		}, function(error){
+			if(error){ console.log("Cannot connect to google."); }
+			else{ console.log("Logged in:"); console.log(Meteor.user()); }
+		});
+	}
+}
+
+Utils = {
+	mstostr: function(milliseconds){
+		function numberEnding (number) {
+			return (number > 1) ? 's' : '';
+		}
+		var temp = Math.floor(milliseconds / 1000);
+		var years = Math.floor(temp / 31536000);
+		if (years && years>=0) {
+			return years + ' year' + numberEnding(years) + ' ago';
+		}
+		var days = Math.floor((temp %= 31536000) / 86400);
+		if (days && days>=0) {
+			return days + ' day' + numberEnding(days) + ' ago';
+		}
+		var hours = Math.floor((temp %= 86400) / 3600);
+		if (hours && hours>=0) {
+			return hours + ' hour' + numberEnding(hours) + ' ago';
+		}
+		var minutes = Math.floor((temp %= 3600) / 60);
+		if (minutes && minutes>=0) {
+			return minutes + ' minute' + numberEnding(minutes) + ' ago';
+		}
+		var seconds = temp % 60;
+		if (seconds && seconds>=0) {
+			return seconds + ' second' + numberEnding(seconds) + ' ago';
+		}
+		return 'just now';
+	}
+}
