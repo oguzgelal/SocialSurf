fdc2886a7ec8c7b875236b2061f83e2bd677d0c2
Oguz Gelal <o.gelal77@gmail.com>
2015-07-03 14:13:40 +0300

    landing page improved

--- client/js/pages/landing.js
+++ client/js/pages/landing.js
@@ -1,5 +1,22 @@
+$(document).ready(function(){
+	$(".nano").nanoScroller({ destroy: true });
+	
+});
+
 Template.landing.rendered = function(){
 	$('body').addClass('landingPage');
+
+	$('body').on("scroll", function(){
+		$(".anim-arrow").show();
+		setTimeout(function(){
+			$(".anim-arrow").fadeOut();
+		},500);
+	});
+
+	$(".anim-br-light").mouseenter(function(){
+
+	});
+
 }
 Template.landing.destroyed = function(){
 	$('body.landingPage').removeClass('landingPage');
